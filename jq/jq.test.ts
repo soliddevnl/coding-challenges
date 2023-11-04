5cc00983a100f2b1a0e6a33d5f6937ab0dc299d3
Joost van Driel <joostvdriel@gmail.com>
2023-11-04 13:52:15 +0100

    Added support for the first argument

--- jq/jq.test.ts
+++ jq/jq.test.ts
@@ -1,18 +1,22 @@
 import { describe, it, expect } from 'vitest'
-import { jq } from './jq'
+import { jq, main } from './jq'
 
 describe('jq', function () {
-  it('should write an empty array when given an empty string', function () {
-    expect(jq('')).toBe('[]')
+  it('should write an empty array when given an empty string', async function () {
+    const result = await jq('', new Set())
+
+    expect(result).toBe('[]')
   })
 
-  it('should format a simple json string correctly', () => {
-    expect(jq('{"foo": "bar"}')).toBe(`{
+  it('should format a simple json string correctly', async () => {
+    const result = await jq('{"foo": "bar"}', new Set())
+
+    expect(result).toBe(`{
   "foo": "bar"
 }`)
   })
 
-  it('should format a complex json string correctly', () => {
+  it('should format a complex json string correctly', async () => {
     const input = '{"quotes":[{"id":1,"quote":"Life isn’t about getting and having, it’s about giving and being.","author":"Kevin Kruse"},{"id":2,"quote":"Whatever the mind of man can conceive and believe, it can achieve.","author":"Napoleon Hill"}],"total":100,"skip":0,"limit":2}'
 
     const expected = `{
@@ -32,6 +36,20 @@ describe('jq', function () {
   "skip": 0,
   "limit": 2
 }`
-    expect(jq(input)).toBe(expected)
+
+    const result = await jq(input, new Set())
+
+    expect(result).toBe(expected)
+  })
+
+  it('should return the given json in the correct format when give the simplest filter \'.\'', async () => {
+    const input = '{"foo": "bar"}'
+    const expected = `{
+  "foo": "bar"
+}`
+
+    const result = await jq(input, new Set(['.']))
+
+    expect(result).toBe(expected)
   })
 })
--- jq/jq.ts
+++ jq/jq.ts
@@ -1,9 +1,32 @@
-export function jq (input: string): string {
+export async function jq (input: string, args: Set<string>): Promise<string> {
   if (input?.length > 0) {
-    return JSON.stringify(JSON.parse(input), null, 2)
+    if (args.size === 0 || args.has('.')) {
+      return JSON.stringify(JSON.parse(input), null, 2)
+    }
   }
 
   return '[]'
 }
 
-jq(process.argv[2])
+async function readInput (): Promise<string> {
+  return await new Promise((resolve, reject) => {
+    let input = ''
+    process.stdin.on('data', (data) => {
+      input += data.toString()
+    })
+    process.stdin.on('end', () => {
+      resolve(input)
+    })
+    return input
+  })
+}
+
+export async function main (): Promise<string> {
+  const args = new Set(process.argv.slice(2))
+  const input = await readInput()
+  const result = await jq(input, args)
+  process.stdout.write(result)
+  return result
+}
+
+void main()
